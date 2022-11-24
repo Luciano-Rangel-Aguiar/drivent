67f60a5d887ef3a938c12e9a04d1f9fc0e41e814
Luciano-Rangel-Aguiar <luciano.rangel.aguiar@gmail.com>
2022-11-24 18:20:53 -0300

    all done

---
+++ src/protocols.ts
@@ -0,0 +1,49 @@
+export type ApplicationError = {
+  name: string;
+  message: string;
+};
+
+export type ViaCEPAddress = {
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  localidade: string,
+  uf: string,
+
+};
+
+export type AddressEnrollment = {
+  logradouro: string,
+  complemento: string,
+  bairro: string,
+  cidade: string,
+  uf: string,
+  error?: string
+
+}
+
+export type RequestError = {
+  status: number,
+  data: object | null,
+  statusText: string,
+  name: string,
+  message: string,
+};
+
+export type TicketType = {
+  id: number,
+  name: string,
+  price: number,
+  isRemote: boolean,
+  includesHotel: boolean,
+  createdAt: Date,
+  updatedAt: Date,
+};
+
+export type Card = {
+  issuer: string,
+  number: number,
+  name: string,
+  expirationDate: Date,
+  cvv: number
+}
